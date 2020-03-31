096d26751c549f6a879e7ad68ea1df6efe958a2d
sbondo1234 <37304121+sbondo1234@users.noreply.github.com>
2020-03-31 22:50:11 +0100

    start making settingsLib
    Also move libs to .net core 3.1

---
+++ Casterr.SettingsLib/GeneralSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Casterr.SettingsLib
+{
+    public class GeneralSettings
+    {
+        public string GetFile()
+        {
+            Paths p = new Paths();
+
+            return p.FilePath("GeneralSettings.json");
+        }
+    }
+}
---
+++ Casterr.SettingsLib/Paths.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casterr.SettingsLib
+{
+    class Paths
+    {
+        private string FolderPath(string folder)
+        {
+            string mainFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Casterr");
+            string folderWanted = Path.Combine(mainFolder, folder);
+            
+            // If folder does not exist, create it
+            if (!Directory.Exists(folderWanted))
+            {
+                Directory.CreateDirectory(folderWanted);
+            }
+
+            // Return main folder if asked for
+            if (folder == "main")
+            {
+                return mainFolder;
+            }
+
+            return folderWanted;
+        }
+
+        public string FilePath(string file)
+        {
+            string settingsFolder = FolderPath("settings");
+            string settingsFile = Path.Combine(settingsFolder, file);
+
+            // If settings file doesn't exist, create it
+            if (!File.Exists(settingsFile))
+            {
+                File.Create(settingsFile);
+            }
+
+            return settingsFile;
+        }
+    }
+}
