bde63c0bf4ff8d0d44cf3605fcb1375ca13be6bd
sbondo1234 <37304121+sbondo1234@users.noreply.github.com>
2020-04-04 12:25:45 +0100

    add RecordingSettings class with base settings
    need to find out how to get screen res with asp

---
+++ Casterr.SettingsLib/RecordingSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace Casterr.SettingsLib
+{
+    public class RecordingSettings
+    {
+        #region Video
+        public int FPS { get; set; } = 60;
+        public string Resolution { get; set; } = "1920x1080";
+        public string Format { get; set; } = "mp4";
+        public bool ZeroLatency { get; set; } = true;
+        public bool UltraFast { get; set; } = true;
+        #endregion
+
+        #region Audio
+        public string AudioDevice { get; set; } = "default";
+        public bool SeperateAudioTracks { get; set; } = false;
+        #endregion
+    }
+}
