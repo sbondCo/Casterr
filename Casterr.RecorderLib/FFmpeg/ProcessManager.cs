c5eae243fe839299d9a534089871b98f12d07545
sbondo1234 <37304121+sbondo1234@users.noreply.github.com>
2020-06-06 21:10:39 +0100

    move process creating to ProcessManager

---
+++ Casterr.RecorderLib/FFmpeg/ProcessManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Casterr.RecorderLib.FFmpeg
+{
+    public class ProcessManager
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool GenerateConsoleCtrlEvent(int dwCtrlEvent, int dwProcessGroupId);
+        [DllImport("kernel32.dll")]
+        public static extern bool SetConsoleCtrlHandler(IntPtr handlerRoutine, bool add);
+        [DllImport("kernel32.dll")]
+        public static extern bool AttachConsole(int dwProcessId);
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeConsole();
+
+        FindFFmpeg ff = new FindFFmpeg();
+
+        private string ffmpegPath;
+        private readonly Process ffProcess = new Process();
+        public string ProcessOutput, ProcessError;
+
+        /// <summary>
+        /// Start new FFmpeg process.
+        /// </summary>
+        /// <param name="args">Arguments to send FFmpeg process.</param>
+        /// <param name="redirectOutput">Should redirect standard output.</param>
+        /// <param name="redirectError">Should redirect standard error.</param>
+        /// <returns></returns>
+        public async Task StartProcess(string args, bool redirectOutput = false, bool redirectError = false)
+        {
+            ffmpegPath = await ff.GetPath();
+
+            if (ffProcess != null)
+            {
+                ffProcess.StartInfo.FileName = ffmpegPath;
+                ffProcess.StartInfo.Arguments = args;
+                ffProcess.StartInfo.CreateNoWindow = false;
+                ffProcess.StartInfo.RedirectStandardOutput = redirectOutput;
+                ffProcess.StartInfo.RedirectStandardError = redirectError;
+                ffProcess.Start();
+
+                Console.WriteLine("Started FFmpeg");
+
+                // Set process[output/error] vars
+                if (redirectOutput)
+                {
+                    var output = ffProcess.StandardOutput.ReadToEnd();
+                    var error = ffProcess.StandardError.ReadToEnd();
+
+                    if (!string.IsNullOrEmpty(output)) ProcessOutput = output;
+                    if (!string.IsNullOrEmpty(error)) ProcessError = error;
+                }
+            }
+            else
+            {
+                throw new RecorderException("Could not start ffmpeg");
+            }
+        }
+
+        /// <summary>
+        /// Stop FFmpeg process.
+        /// </summary>
+        public void StopProcess()
+        {
+            try
+            {
+                if (ffProcess != null)
+                {
+                    Console.WriteLine("Stopping FFmpeg");
+
+                    AttachConsole(ffProcess.Id);
+                    SetConsoleCtrlHandler(IntPtr.Zero, true);
+                    GenerateConsoleCtrlEvent(0, 0);
+
+                    Thread.Sleep(2000);
+
+                    SetConsoleCtrlHandler(IntPtr.Zero, false);
+                    FreeConsole();
+
+                    Console.WriteLine("Stopped FFmpeg");
+                }
+                else
+                {
+                    throw new RecorderException("Can't stop recording, when not already recording.");
+                }
+            }
+            catch (Exception)
+            {
+                throw new RecorderException("Can't stop recording, when not already recording.");
+            }
+        }
+    }
+}
--- Casterr.RecorderLib/Recorder.cs
+++ Casterr.RecorderLib/Recorder.cs
@@ -1,77 +1,33 @@
 ﻿using Casterr.RecorderLib.FFmpeg;
 using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Casterr.RecorderLib
 {
     public class Recorder
     {
-        [DllImport("kernel32.dll")]
-        public static extern bool GenerateConsoleCtrlEvent(int dwCtrlEvent, int dwProcessGroupId);
-        [DllImport("kernel32.dll")]
-        public static extern bool SetConsoleCtrlHandler(IntPtr handlerRoutine, bool add);
-        [DllImport("kernel32.dll")]
-        public static extern bool AttachConsole(int dwProcessId);
-        [DllImport("kernel32.dll")]
-        public static extern bool FreeConsole();
+        ProcessManager process = new ProcessManager();
 
-        FindFFmpeg ff = new FindFFmpeg();
-
-        string ffmpegPath;
-        private Process ffProcess = new Process();
-
+        /// <summary>
+        /// Start FFmpeg process and send arguments to it.
+        /// </summary>
+        /// <param name="args">Arguments to send FFmpeg.</param>
+        /// <returns></returns>
         public async Task Start(string args)
         {
-            ffmpegPath = await ff.GetPath();
-            //string finalPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyVideos)}\\output.mkv";
-            //string args = $@"-f dshow -i video='UScreenCapture':audio='Microphone' {finalPath}";
+            // Append finalPath to the end of arguments
+            string finalPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyVideos)}\\output.mkv";
+            args = $"{args} {finalPath}";
 
-            if (ffProcess != null)
-            {
-                ffProcess.StartInfo.FileName = ffmpegPath;
-                ffProcess.StartInfo.Arguments = args;
-                ffProcess.StartInfo.CreateNoWindow = false;
-                ffProcess.Start();
-
-                Console.WriteLine("Started FFmpeg");
-            }
-            else
-            {
-                throw new RecorderException("Could not start ffmpeg");
-            }
+            await process.StartProcess(args);
         }
 
+        /// <summary>
+        /// Stop FFmpeg process.
+        /// </summary>
         public void Stop()
         {
-            try
-            {
-                if (ffProcess != null)
-                {
-                    Console.WriteLine("Stopping FFmpeg");
-
-                    AttachConsole(ffProcess.Id);
-                    SetConsoleCtrlHandler(IntPtr.Zero, true);
-                    GenerateConsoleCtrlEvent(0, 0);
-
-                    Thread.Sleep(2000);
-
-                    SetConsoleCtrlHandler(IntPtr.Zero, false);
-                    FreeConsole();
-
-                    Console.WriteLine("Stopped FFmpeg");
-                }
-                else
-                {
-                    throw new RecorderException("Can't stop recording, when not already recording");
-                }
-            }
-            catch (Exception)
-            {
-                throw new RecorderException("Can't stop recording, when not already recording");
-            }
+            process.StopProcess();
         }
     }
 }
