c1c4af3ce27ed9fd1ec26916e739ee3c361e752f
sbondo1234 <37304121+sbondo1234@users.noreply.github.com>
2020-07-19 16:05:51 +0100

    Create GetRecordings Class

---
+++ Casterr.HelpersLib/GetRecordings.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Casterr.HelpersLib
+{
+    public class RecordingPaths
+    {
+        public string VideoPath { get; set; }
+        public string ThumbPath { get; set; }
+    }
+
+    public static class GetRecordings
+    {
+        public static List<RecordingPaths> Get(string videoPath, string thumbPath)
+        {
+            List<RecordingPaths> ri = new List<RecordingPaths>();
+            
+            foreach (string f in Directory.GetFiles(videoPath, "*.mp4", SearchOption.AllDirectories))
+            {
+                // Path that should be to the thumbnail, unless it has been moved/name changed
+                string tPath = $"{thumbPath}\\{Path.GetFileName(f)}.png";
+
+                // If thumbnail does not exist, make it an empty string for List
+                if (!File.Exists(tPath))
+                {
+                    tPath = "";
+                }
+
+                // Add paths to list
+                ri.Add(new RecordingPaths { VideoPath = f, ThumbPath = tPath});
+            }
+
+            return ri;
+        }
+    }
+}
