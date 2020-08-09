113c3fa6c51052e59182d1a95ba3464d68299fc8
IRHM <37304121+IRHM@users.noreply.github.com>
2020-08-09 02:44:22 +0100

    FFmpeg downloading works unix
    cant test mac.
    
    Also make find/download ffmpeg classes static

--- Casterr.RecorderLib/FFmpeg/FindFFmpeg.cs
+++ Casterr.RecorderLib/FFmpeg/FindFFmpeg.cs
@@ -1,33 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Runtime.InteropServices;
 using System.IO;
+using System;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Casterr.RecorderLib.FFmpeg
 {
-    class FindFFmpeg
-    {
-        const string FFmpegExeName = "ffmpeg.exe";
-
-        /// <summary>
-        /// Get path to FFmpeg exe.
-        /// </summary>
-        /// <returns>Path to FFmpeg exe.</returns>
-        public async Task<string> GetPath()
+  public static class FindFFmpeg
+  {
+    /// <summary>
+    /// FFmpeg executable name
+    /// </summary>
+    /// <value>FFmpeg executable name</value>
+    static string FFmpegExeName
+    { 
+      get
+      {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            string execPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            string ffmpegPath = $"{execPath}\\{FFmpegExeName}";
-
-            // If FFmpegPath does not exist, go download it
-            if (!File.Exists(ffmpegPath))
-            {
-                DownloadFFmpeg df = new DownloadFFmpeg();
-                await df.Download(ffmpegPath, FFmpegExeName);
-            }
-
-            return ffmpegPath;
+          // Windows needs .exe extension
+          return "ffmpeg.exe";
         }
+        else
+        {
+          // Linux & Mac need no extension
+          return "ffmpeg";
+        }
+      } 
     }
+
+    /// <summary>
+    /// Get path to FFmpeg exe.
+    /// </summary>
+    /// <returns>Path to FFmpeg exe.</returns>
+    public static async Task<string> GetPath()
+    {
+      string execPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+      string ffmpegPath = Path.Combine(execPath, FFmpegExeName);
+
+      // If FFmpegPath does not exist, go download it
+      if (!File.Exists(ffmpegPath))
+      {
+        // DownloadFFmpeg df = new DownloadFFmpeg();
+        await DownloadFFmpeg.Download(ffmpegPath, FFmpegExeName);
+      }
+
+      return ffmpegPath;
+    }
+  }
 }
