2fdad9b82563e45014be604e5166c24ed559f578
IRHM <37304121+IRHM@users.noreply.github.com>
2020-08-19 22:29:21 +0100

    Send 'q' to stop ffmpeg instead of control+c
    Process wasn't stopping when using control+c, luckily ffmpeg can stop using 'q'

--- Casterr.RecorderLib/FFmpeg/ProcessManager.cs
+++ Casterr.RecorderLib/FFmpeg/ProcessManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
 using Casterr.HelpersLib;
 
@@ -42,6 +41,7 @@ namespace Casterr.RecorderLib.FFmpeg
         ffProcess.StartInfo.CreateNoWindow = true;
         ffProcess.StartInfo.RedirectStandardOutput = redirectOutput;
         ffProcess.StartInfo.RedirectStandardError = redirectError;
+        ffProcess.StartInfo.RedirectStandardInput = true;
         ffProcess.Start();
 
         Console.WriteLine("Started FFmpeg");
@@ -75,37 +75,26 @@ namespace Casterr.RecorderLib.FFmpeg
     /// </summary>
     public void StopProcess()
     {
-      try
+      if (ffProcess != null)
       {
-        if (ffProcess != null)
+        Console.WriteLine("Stopping FFmpeg");
+
+        try
         {
-          Console.WriteLine("Stopping FFmpeg");
+          // Send 'q' to ffmpeg process, which will make it quit
+          ffProcess.StandardInput.WriteLine("q");
 
-          // Try closing process twice
-          // Temporary solution
-          int i = 0;
-          while (i <= 1)
-          {
-            AttachConsole(ffProcess.Id);
-            SetConsoleCtrlHandler(IntPtr.Zero, true);
-            GenerateConsoleCtrlEvent(0, 0);
-
-            Thread.Sleep(2000);
-
-            SetConsoleCtrlHandler(IntPtr.Zero, false);
-            FreeConsole();
-
-            i++;
-          }
-
-          Console.WriteLine("Stopped FFmpeg");
+          // Wait for ffmpeg process to exit
+          ffProcess.WaitForExit();
         }
-        else
+        catch (Exception)
         {
-          throw new RecorderException("Can't stop recording, when not already recording.");
+          throw new RecorderException("Error trying to stop recording.");
         }
+
+        Console.WriteLine("Stopped FFmpeg");
       }
-      catch (Exception)
+      else
       {
         throw new RecorderException("Can't stop recording, when not already recording.");
       }
