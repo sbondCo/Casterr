45be955dc9c3146df6a801ea426560e207d1fafb
DanyaalA <Danny13653@gmail.com>
2020-08-20 13:28:35 +0100

    Removed Comments

--- Casterr.RecorderLib/FFmpeg/FindFFmpeg.cs
+++ Casterr.RecorderLib/FFmpeg/FindFFmpeg.cs
@@ -35,9 +35,7 @@ namespace Casterr.RecorderLib.FFmpeg
     /// <returns>Path to FFmpeg exe.</returns>
     public static async Task<string> GetPath()
     {
-      Console.WriteLine("#1");
       string execPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-      Console.WriteLine("#2");
 
       string ffmpegPath = Path.Combine(execPath, FFmpegExeName);
       Console.WriteLine(ffmpegPath);
@@ -45,11 +43,8 @@ namespace Casterr.RecorderLib.FFmpeg
       // If FFmpegPath does not exist, go download it
       if (!File.Exists(ffmpegPath))
       {
-        Console.WriteLine("#3");
-
         // DownloadFFmpeg df = new DownloadFFmpeg();
         await DownloadFFmpeg.Download(ffmpegPath, FFmpegExeName);
-        Console.WriteLine("#4");
       }
 
       return ffmpegPath;
