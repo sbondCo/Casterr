7c0ecd8e7c60e7349ecdc7f6f706b8796ebe7dd1
IRHM <37304121+IRHM@users.noreply.github.com>
2020-08-23 18:14:34 +0100

    Create MediaLib and Get Recordings
    Also add video thumbnails to recordings

---
+++ Casterr.MediaLib/Recording.cs
@@ -0,0 +1,11 @@
+namespace Casterr.MediaLib
+{
+  public class Recording
+  {
+    public string VideoPath { get; set; }
+    
+    #nullable enable
+    public string? ThumbPath { get; set; }
+    #nullable disable
+  }
+}
---
+++ Casterr.MediaLib/Recordings.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Casterr.SettingsLib;
+
+namespace Casterr.MediaLib
+{
+  public static class Recordings
+  {
+    public static List<Recording> Get()
+    {
+      List<Recording> recordings = new List<Recording>();
+      SettingsManager sm = new SettingsManager();
+      RecordingSettings rs = new RecordingSettings();
+
+      sm.GetSettings(rs);
+
+      var videos = Directory
+        .EnumerateFiles(rs.VideoSaveFolder)
+        .Where(v => v.ToLower().EndsWith("mp4") || v.ToLower().EndsWith("mkv"))
+        .ToList();
+
+      foreach (var v in videos)
+      {
+        var thumbPath = Path.Combine(rs.ThumbSaveFolder, $"{Path.GetFileName(v)}.png");
+
+        recordings.Add(new Recording {
+          VideoPath = v,
+          ThumbPath = (File.Exists(thumbPath)) ? thumbPath : null
+        });
+      }
+
+      return recordings;
+    }
+  }
+}
