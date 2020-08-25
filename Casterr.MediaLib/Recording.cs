ffc425c92a118046be2f901b4cff51d77c5d843c
IRHM <37304121+IRHM@users.noreply.github.com>
2020-08-25 18:50:19 +0100

    Get video duration and fps from ffprobe
    Going to keep this in, but do it only one per video after recording, then save it in a json file.

--- Casterr.MediaLib/Recording.cs
+++ Casterr.MediaLib/Recording.cs
@@ -3,7 +3,9 @@ namespace Casterr.MediaLib
   public class Recording
   {
     public string VideoPath { get; set; }
-    public long FileSize { get; set; } = 0;
     public string ThumbPath { get; set; }
+    public long FileSize { get; set; } = 0;
+    public string FPS { get; set; } = null;
+    public string Duration { get; set; } = null;
   }
 }
--- Casterr.MediaLib/Recordings.cs
+++ Casterr.MediaLib/Recordings.cs
@@ -1,14 +1,16 @@
 using System;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Casterr.SettingsLib;
+using Casterr.RecorderLib.FFmpeg;
 
 namespace Casterr.MediaLib
 {
   public static class Recordings
   {
-    public static List<Recording> Get()
+    public static async Task<List<Recording>> Get()
     {
       List<Recording> recordings = new List<Recording>();
       SettingsManager sm = new SettingsManager();
@@ -25,15 +27,50 @@ namespace Casterr.MediaLib
       {
         var fi = new FileInfo(v);
         var thumbPath = Path.Combine(rs.ThumbSaveFolder, $"{Path.GetFileName(v)}.png");
+        var videoInfo = await GetVideoInfo(v);
 
         recordings.Add(new Recording {
           VideoPath = v,
           ThumbPath = (File.Exists(thumbPath)) ? thumbPath : null,
-          FileSize = fi.Length
+          FileSize = fi.Length,
+          FPS = (videoInfo.TryGetValue("fps", out var fps)) ? fps : "N/A",
+          Duration = (videoInfo.TryGetValue("duration", out var duration)) ? duration : "N/A"
         });
       }
 
       return recordings;
+    }
+
+    private static async Task<Dictionary<string, string>> GetVideoInfo(string video)
+    {
+      var ff = new ProcessManager();
+      var d = new Dictionary<string, string>();
+
+      // Query ffprobe to get video duration and fps
+      var info = await ff.StartProcess(
+        $"-v error -select_streams v:0 -show_entries format=duration -sexagesimal -show_entries stream=avg_frame_rate -of default=noprint_wrappers=1 \"{video}\"",
+        "ffprobe",
+        true,
+        true
+      );
+
+      // Loop over each line in response from ffprobe
+      foreach (var line in info.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+      {
+        // Get framerate
+        if (line.ToLower().Contains("avg_frame_rate"))
+        {
+          d.Add("fps", line.ToLower().Replace("avg_frame_rate=", ""));
+        }
+
+        // Get duration
+        if (line.ToLower().Contains("duration"))
+        {
+          d.Add("duration", line.ToLower().Replace("duration=", ""));
+        }
+      }
+
+      return d;
     }
   }
 }
