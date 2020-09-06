8f09d93182f5f1de718aa03fa8a3cbde92b35ed8
IRHM <37304121+IRHM@users.noreply.github.com>
2020-09-06 20:30:15 +0100

    Percentage done for extraction of ffmpeg/ffprobe

--- Casterr.RecorderLib/FFmpeg/DownloadFFmpeg.cs
+++ Casterr.RecorderLib/FFmpeg/DownloadFFmpeg.cs
@@ -53,6 +53,7 @@ namespace Casterr.RecorderLib.FFmpeg
 
       ZipPath = Path.Combine(Path.GetTempPath(), "ffmpeg.zip");
 
+      // Update programStatus on ProgressChanged
       DownloadHelper.ProgressChanged += (progress => { ProgramStatus.DoingSomething(true, "Downloading FFmpeg", progress); });
       await DownloadHelper.Download(DownloadUri, ZipPath);
     }
@@ -74,6 +75,7 @@ namespace Casterr.RecorderLib.FFmpeg
 
         // Get ffmpeg and ffprobe from zip
         var ffFiles = zip.Entries.Where(f => f.Name == FindFFmpeg.FFmpegExeName || f.Name == FindFFmpeg.FFprobeExeName);
+        var iteration = 1;
 
         // Extract ffFiles to folder current app is being run from
         foreach (var f in ffFiles)
@@ -87,6 +89,10 @@ namespace Casterr.RecorderLib.FFmpeg
           {
             f.ExtractToFile(Path.Combine(dest));
           }
+
+          // Set progess percentage to amount of files extracted
+          ProgramStatus.DoingSomething(true, "Extracting FFmpeg", (int) (Decimal.Divide(iteration, ffFiles.Count()) * 100));
+          iteration++;
         }
 
         // Dispose and delete zip file
