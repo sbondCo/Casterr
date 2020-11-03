56f4d8c922cdafefdf357ea6d25a2b84d7183273
IRHM <37304121+IRHM@users.noreply.github.com>
2020-11-03 19:27:50 +0000

    Fix index.html not being loaded in build of app
    For some reason using app:// protocol here doesn't work. Still need to register the protocol for other parts of the app to work.

--- Casterr/src/background.ts
+++ Casterr/src/background.ts
@@ -51,7 +51,7 @@ async function createWindow() {
     createProtocol('app')
 
     // Load the index.html when not in development
-    win.loadURL('app://index.html')
+    win.loadURL(`file://${path.join(__dirname, 'index.html')}`) 
   }
 }
 
