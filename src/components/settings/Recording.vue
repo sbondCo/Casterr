85270d1704d3bee1e9bc03f1d8367e42377bcbf7
IRHM <37304121+IRHM@users.noreply.github.com>
2020-11-14 16:16:32 +0000

    Add all settings to Recording settings component
    Requires some components that havent been created

--- src/components/settings/Recording.vue
+++ src/components/settings/Recording.vue
@@ -1,11 +1,93 @@
 <template>
-  <span>RecordingSettings</span>
+    <div class="settings">
+    <span class="pageTitle">Recording Settings</span>
+
+    <div class="setting">
+      <span class="title">Video Device:</span>
+      <DropDown name="videoDevice" :placeholder="videoDevice" :items="videoDevices" @itemChanged="updateSettings" />
+    </div>
+
+    <div class="setting">
+      <span class="title">FPS:</span>
+      <!-- <TextBox name="fps" :placeholder="" type="number" @itemChanged="updateSettings" /> -->
+    </div>
+
+    <div class="setting">
+      <span class="title">Resolution:</span>
+      <DropDown name="resolution" :placeholder="resolution" :items="resolutions" @itemChanged="updateSettings" />
+    </div>
+
+    <div class="setting">
+      <span class="title">Format:</span>
+      <DropDown name="format" :placeholder="format" :items="formats" @itemChanged="updateSettings" />
+    </div>
+
+    <div v-if="this.isWindows" class="setting">
+      <span class="title">Zero Latency:</span>
+      <!-- <TickBox name="zeroLatency" :checked="zeroLatency" @itemChanged="updateSettings" /> -->
+    </div>
+
+    <div v-if="this.isWindows" class="setting">
+      <span class="title">Ultra Fast:</span>
+      <!-- <TickBox name="ultraFast" :checked="ultraFast" @itemChanged="updateSettings" /> -->
+    </div>
+
+    <div class="setting">
+      <span class="title">Audio Devices To Record:</span>
+      <!-- <ListBox name="audioDevicesToRecord"  @itemChanged="updateSettings" /> -->
+    </div>
+
+    <div class="setting">
+      <span class="title">Seperate Audio Tracks:</span>
+      <!-- <TickBox name="seperateAudioTracks"  @itemChanged="updateSettings" /> -->
+    </div>
+
+    <div class="setting">
+      <span class="title">Thumbnail Save Folder:</span>
+      <!-- <TextBox name="thumbSaveFolder"  @itemChanged="updateSettings" /> -->
+    </div>
+
+    <div class="setting">
+      <span class="title">Video Save Folder:</span>
+      <!-- <TextBox name="videoSaveFolder"  @itemChanged="updateSettings" /> -->
+    </div>
+
+    <div class="setting">
+      <span class="title">Video Save Name:</span>
+      <!-- <TextBox name="videoSaveName"  @itemChanged="updateSettings" /> -->
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { Component, Vue } from "vue-property-decorator";
+import DropDown from "./../ui/DropDown.vue";
+import { RecordingSettings } from "./../../ts/settings";
+import "../../ts/helpers/extensions/ArrayExtensions";
 
-export default class RecordingSettingsComponent extends Vue{}
+@Component({
+  components: {
+    DropDown,
+  },
+})
+
+export default class RecordingSettingsComponent extends Vue {
+  data() {
+    return {
+      isWindows: (require("os").platform == 'win32'),
+      videoDevice: "Some Mic",
+      videoDevices: ["Some Mic", "A TV?", "Is this a headset?", "G40${randNum}"],
+      resolution: RecordingSettings.resolution,
+      resolutions: ["In-Game", "2160p", "1440p", "1080p", "720p", "480p", "360p"],
+      format: RecordingSettings.format,
+      formats: ["mp4", "mkv"]
+    }
+  }
+
+  updateSettings(toUpdate: string, newValue: string) {
+    console.log(toUpdate + ' ' + newValue);
+  }
+}
 </script>
 
 <style lang="scss">
