ff8ed5f0806f5f0c0c321209bbfd5528efcd2a9b
IRHM <37304121+IRHM@users.noreply.github.com>
2020-11-18 16:16:03 +0000

    Setup getPath and download helper
    Need to refine it before it is completed fully

---
+++ src/libs/helpers/downloader.ts
@@ -0,0 +1,44 @@
+import * as https from "https";
+import * as fs from "fs";
+import * as path from "path";
+const jsZip = require('jszip');
+
+export default class Downloader {
+  /**
+   * Download a file
+   * @param uri URI to download file from
+   * @param dest Destination path for downloaded file
+   */
+  public static get(uri: string, dest: string) {
+    const file = fs.createWriteStream(dest);
+
+    https.get(uri, (resp) => {
+      resp.pipe(file);
+    });
+  }
+
+  public static extract(zipPath: string, destFolder: string) {
+    let options = {
+      autoClose: true,
+      lazyEntries: true,
+      decodeStrings: true,
+      validateEntrySizes: true,
+      strictFileNames: false
+    };
+
+    fs.readFile(zipPath, (err, data) => {
+      if (!err) {
+        var zip = new jsZip();
+        zip.loadAsync(data).then((contents: any) => {
+          Object.keys(contents.files).forEach((filename) => {
+            console.log(filename);
+
+            zip.file(filename).async('nodebuffer').then((content: any) => {
+              fs.writeFileSync(path.join(destFolder, filename), content);
+            });
+          });
+        });
+      }
+    });
+  }
+}
--- src/libs/recorder/ffmpeg.ts
+++ src/libs/recorder/ffmpeg.ts
@@ -1,6 +1,28 @@
+import * as path from "path";
+import * as fs from "fs";
+import Downloader from "./../helpers/downloader";
+
 export default class FFmpeg {
   constructor(private which = "ffmpeg") {
 
+  }
+
+  public static get ffmpegExeName() {
+    if (process.platform == 'win32') {
+      return "ffmpeg.exe"
+    }
+    else {
+      return "ffmpeg"
+    }
+  }
+
+  public static get ffprobeExeName() {
+    if (process.platform == 'win32') {
+      return "ffprobe.exe"
+    }
+    else {
+      return "ffprobe"
+    }
   }
 
   public run() {
@@ -9,6 +31,26 @@ export default class FFmpeg {
   }
 
   public getPath() {
-    console.log(process.execPath);
+    let execPath = path.dirname(process.execPath);
+    let ffmpegPath = path.join(execPath, FFmpeg.ffmpegExeName);
+    let ffprobePath = path.join(execPath, FFmpeg.ffprobeExeName);
+
+    console.log(path.dirname(process.execPath));
+
+    // If ffmpeg or ffprobe does not exist, go download it
+    if (!fs.existsSync(ffmpegPath) || !fs.existsSync(ffprobePath)) {
+      let downloadTo = ffmpegPath + '.zip';
+      Downloader.get("https://ul.sbond.co/ffmpeg/ffmpeg-release-linux-amd64.zip", downloadTo);
+      Downloader.extract(downloadTo, execPath);
+    }
+
+    // Return path to executable
+    if (this.which == "ffprobe")
+    {
+      return ffprobePath;
+    }
+    
+    // Always return ffmpegPath, unless something gets returned in if statement above
+    return ffmpegPath;
   }
 }
