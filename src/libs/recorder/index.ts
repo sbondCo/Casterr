b5771968f4829e4b904024bda6efbf3b0ba1eb0a
IRHM <37304121+IRHM@users.noreply.github.com>
2020-11-19 19:21:04 +0000

    Only start recording if not currently doing so

--- src/libs/recorder/index.ts
+++ src/libs/recorder/index.ts
@@ -2,16 +2,23 @@ import FFmpeg from "./ffmpeg";
 
 export default class Recorder {
   private static ffmpeg = new FFmpeg();
+  private static isRecording: Boolean = false;
 
   public static start() {
-    this.ffmpeg.run("-f x11grab -i :0.0+0,0 /home/sbondo/Videos/Casterr/output.mkv", {
-      stderrCallback: (err: string) => {
-        console.log(err);
-      }
-    });
+    // Only start recording if not currently doing so
+    if (this.isRecording == false) {
+      this.ffmpeg.run("-f x11grab -i :0.0+0,0 /home/sbondo/Videos/Casterr/output.mkv", {
+        stderrCallback: (err: string) => {
+          console.log(err);
+        }
+      });
+
+      this.isRecording = true;
+    }
   }
 
   public static stop() {
     this.ffmpeg.kill();
+    this.isRecording = false;
   }
 }
