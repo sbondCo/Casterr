ba8584599fb251933d11d426c225a8fc9d924abe
IRHM <37304121+IRHM@users.noreply.github.com>
2020-11-26 00:06:30 +0000

    Finish days section in toReadableDateTime

--- src/libs/helpers/extensions/stringExtensions.ts
+++ src/libs/helpers/extensions/stringExtensions.ts
@@ -7,7 +7,31 @@ interface String {
  * Convert string of time codes to readable format.
  */
 String.prototype.toReadableDateTime = function (this): string {
-  return "hi";
+  let rdt = this;
+  let now = new Date();
+
+  //#region DAY
+
+  // Day of the month, 01 to 31
+  if (this.includes("%d")) {
+    rdt = rdt.replace("%d", now.getDate().toString());
+  }
+
+  // Abbreviated day of the week, Mon to Sun
+  if (this.includes("%D"))
+  {
+    rdt = rdt.replace("%D", now.toLocaleString(undefined, { weekday: 'short' }));
+  }
+
+  // Full day of the week, Monday to Sunday (%l = lowercase L)
+  if (this.includes("%l"))
+  {
+    rdt = rdt.replace("%l", now.toLocaleString(undefined, { weekday: 'long' }));
+  }
+
+  //#endregion
+
+  return rdt.toString();
 }
 
 /**
