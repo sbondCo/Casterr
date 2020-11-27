478391c2a96fbdef0053fd1358c201fb5686c546
Mr <37304121+IRHM@users.noreply.github.com>
2020-11-27 21:33:25 +0000

    mainFolderPath changed to be inside users Documents folder

--- src/libs/helpers/pathHelper.ts
+++ src/libs/helpers/pathHelper.ts
@@ -3,7 +3,7 @@ const path = require("path");
 
 export default class PathHelper {
   public static get mainFolderPath() {
-    return path.join(os.homedir(), 'Casterr');
+    return path.join(os.homedir(), 'Documents', 'Casterr');
   }
 
   public static get settingsFolderPath() {
