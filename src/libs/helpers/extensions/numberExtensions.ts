16113597903144c3aed8253938c99830ea28fc73
IRHM <37304121+IRHM@users.noreply.github.com>
2020-12-01 00:35:38 +0000

    Make toReadableFromSeconds more readable

--- src/libs/helpers/extensions/numberExtensions.ts
+++ src/libs/helpers/extensions/numberExtensions.ts
@@ -8,12 +8,13 @@ Number.prototype.toReadableFromSeconds = function (this: number): string {
   let h = Math.floor(this % (3600*24) / 3600);
   let m = Math.floor(this % 3600 / 60);
   let s = Math.floor(this % 60);
+  
+  // Turn into readable format
+  let dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+  let hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+  let mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+  let sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
 
-  // Return in readable format
-  return `
-    ${d > 0 ? d + (d == 1 ? " day, " : " days, ") : ""}
-    ${h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : ""}
-    ${m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : ""}
-    ${s > 0 ? s + (s == 1 ? " second" : " seconds") : ""}
-  `;
+  // Return readable format
+  return dDisplay + hDisplay + mDisplay + sDisplay;
 }
