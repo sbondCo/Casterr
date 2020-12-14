14e623bf9d16dfbaa200502acc5df3170d6b3c0f
IRHM <37304121+IRHM@users.noreply.github.com>
2020-12-14 17:59:39 +0000

    Comment Notifications.popup func

--- src/libs/helpers/notifications.ts
+++ src/libs/helpers/notifications.ts
@@ -4,19 +4,33 @@ import Notifier from "./../../components/Notifier.vue";
 export default class Notifications {
   private static activeNotifs = new Map<string, Notifier>();
 
+  /**
+   * Create popup notification.
+   * @param name Name of notification.
+   * @param done If done with notification. True will remove it's instance.
+   * @param percentage Optional percentage for notifications requiring percentage bar.
+   */
   public static popup(name: string, done: boolean, percentage?: number) {
     if (done) {
+      // Get Notifier from activeNotifs map
       const i = this.activeNotifs.get(name);
+
+      // Cleanup component and remove from DOM
       i?.$destroy();
       i?.$el.remove();
+
+      // Delete from activeNotifcs
       this.activeNotifs.delete(name);
+
       return;
     }
 
+    // Update or create notification depending on if it is in activeNotifs
     if (this.activeNotifs.has(name)) {
       const i = this.activeNotifs.get(name);
       if (i != undefined) i.$data.percent = percentage;
     } else {
+      // Create Notifier instance
       const notifier = Vue.extend(Notifier);
       const instance = new notifier({
         propsData: {
@@ -25,9 +39,11 @@ export default class Notifications {
         }
       });
   
+      // Mount and append to DOM in notifications section
       instance.$mount();
       document.getElementById("notifications")?.appendChild(instance.$el);
 
+      // Add to activeNotifs
       this.activeNotifs.set(name, instance);
     }
   }
