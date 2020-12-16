4ef3b33f9d1e4dee4e9d2a49989c60116222926d
IRHM <37304121+IRHM@users.noreply.github.com>
2020-12-16 15:30:24 +0000

    Uncomment margin on .title in notifier

--- src/components/Notifier.vue
+++ src/components/Notifier.vue
@@ -66,7 +66,7 @@ export default class Notifier extends Vue {
     .title {
       font-size: 25px;
       padding: 5px;
-      // margin-bottom: 20px;
+      margin-bottom: 20px;
       text-transform: capitalize;
     }
   }
