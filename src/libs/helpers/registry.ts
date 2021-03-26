3b7832d4c2d564524fef9bea98faab6c683dfb1c
Mr <37304121+IRHM@users.noreply.github.com>
2021-03-26 21:44:30 +0000

    Create Registry class
    Currently just so creating registries are possible since screen-capture-recorder used registry entries for some settings

---
+++ src/libs/helpers/registry.ts
@@ -0,0 +1,32 @@
+import * as childProcess from "child_process";
+
+/**
+ * Manage registry entries on Windows.
+ * TODO: Throw error if initialised on non-windows machine.
+ */
+export default class Registry {
+  constructor(private path: string) {}
+
+  public addReg(name: string, value: string | number, type: keyof RegistryTypes) {
+    console.log(`add ${this.path} /v ${name} /t ${type} /d ${value}`);
+    this.run(`add ${this.path} /v ${name} /t ${type} /d ${value} /f`);
+  }
+
+  private run(cmd: string) {
+    const cp = childProcess.exec(`reg ${cmd}`);
+
+    cp.stdout!.on("data", (data) => console.log(data));
+    cp.stderr!.on("data", (data) => console.log(data));
+  }
+}
+
+/**
+ * Types of registry entries.
+ * Add more when needed: https://docs.microsoft.com/en-us/windows/win32/sysinfo/registry-value-types
+ */
+export interface RegistryTypes {
+  REG_BINARY: string;
+  REG_DWORD: string;
+  REG_NONE: string;
+  REG_QWORD: string;
+}
