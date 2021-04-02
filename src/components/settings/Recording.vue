385f4511bc065dd94f5baca9da2fd599deae3e8b
Mr <37304121+IRHM@users.noreply.github.com>
2021-04-02 04:21:57 +0000

    Fix monitorToRecord dropdown (#142)
    * Use findIndex instead of indexOf so find objects
    
    Support replacing objects in .replace extension method
    
    * Keep selected item in dropdown and highlight it as current active item
    
    instead of just removing it from the array.

--- src/components/settings/Recording.vue
+++ src/components/settings/Recording.vue
@@ -92,7 +92,7 @@ export default class RecordingSettingsComponent extends Vue {
   videoDevice = RecordingSettings.videoDevice;
   videoDevices = ["Default"];
   monitors = new Array<DropDownItem>({ id: "primary", name: "Primary Monitor" });
-  monitorToRecord = RecordingSettings.monitorToRecord.name;
+  monitorToRecord = RecordingSettings.monitorToRecord;
   fps = RecordingSettings.fps;
   resolution = RecordingSettings.resolution;
   resolutions = ["In-Game", "2160p", "1440p", "1080p", "720p", "480p", "360p"];
