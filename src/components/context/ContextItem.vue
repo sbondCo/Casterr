1244a84f6f3fd5d62cdfb9b54a0161449fa2b4a4
IRHM <37304121+IRHM@users.noreply.github.com>
2021-05-30 02:57:10 +0100

    Create ContextMenu and ContextItem

---
+++ src/components/context/ContextItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class ContextMenu extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+div {
+  padding: 8px;
+  border-radius: 3px;
+
+  &:hover {
+    background-color: $secondaryColor;
+  }
+}
+</style>
