f1aa4cd6e6514b38af8686b223603f456f7372b4
IRHM <37304121+IRHM@users.noreply.github.com>
2022-05-30 06:30:08 +0100

    Add direction functionality to icon component and set fill to current color on all paths

--- src/common/Icon.tsx
+++ src/common/Icon.tsx
@@ -11,11 +11,17 @@ interface IconProps {
   className?: string;
 }
 
-export default function Icon({ i, wh = 24, className }: IconProps) {
+export default function Icon({ i, wh = 24, direction, className }: IconProps) {
   const icon = getIcon(i);
 
+  const dirClass = () => {
+    if (direction === "up") return "transform -rotate-180";
+    else if (direction === "down") return "transform rotate-90";
+    else if (direction === "left") return "transform rotate-180";
+  };
+
   return (
-    <svg width={wh} height={wh} viewBox={icon.viewBox} className={className}>
+    <svg width={wh} height={wh} viewBox={icon.viewBox} className={`${className} ${dirClass()}`}>
       {icon.el}
     </svg>
   );
@@ -32,6 +38,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
           <path
             fillRule="evenodd"
             clipRule="evenodd"
+            fill="currentcolor"
             d="M11.5 7L0.5 14V0L11.5 7ZM7.77002 7L2.5 3.64V10.36L7.77002 7Z"
           />
         )
@@ -39,12 +46,21 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
     case "pause":
       return {
         viewBox: "0 0 35 41",
-        el: <path fillRule="evenodd" clipRule="evenodd" d="M0 41H11.6667V0H0V41ZM23.3333 0V41H35V0H23.3333Z" />
+        el: (
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            fill="currentcolor"
+            d="M0 41H11.6667V0H0V41ZM23.3333 0V41H35V0H23.3333Z"
+          />
+        )
       };
     case "upload":
       return {
         viewBox: "0 0 16 18",
-        el: <path fillRule="evenodd" clipRule="evenodd" d="M0 2V0H16V2H0ZM7 8H4L8 4L12 8H9V18H7V8Z" />
+        el: (
+          <path fillRule="evenodd" clipRule="evenodd" fill="currentcolor" d="M0 2V0H16V2H0ZM7 8H4L8 4L12 8H9V18H7V8Z" />
+        )
       };
     case "volumeMute":
       return {
@@ -53,6 +69,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
           <path
             fillRule="evenodd"
             clipRule="evenodd"
+            fill="currentcolor"
             d="M3.886 0L0 3.889l12.018 12.024-.8.827H.193v16.547h11.025L25 47.077V28.902L36.522
             40.43c-1.792 1.352-3.804 2.427-6.01 3.062v5.68c3.694-.827 7.084-2.537 9.951-4.826L46.113
             50 50 46.111 3.886 0zm15.601 33.756l-5.981-5.985h-7.8v-5.515h7.8l2.426-2.427 3.555
@@ -69,6 +86,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
           <path
             fillRule="evenodd"
             clipRule="evenodd"
+            fill="currentcolor"
             d="M0 14.448v17.104h11.111L25 45.805V.195L11.111
             14.448H0zm19.444-.484v18.073l-6.027-6.186H5.556V20.15h7.86l6.028-6.186zm11.111-2.452C34.667
             13.622 37.5 17.954 37.5 23c0 5.046-2.833 9.379-6.945 11.46V11.512z"
@@ -82,6 +100,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
           <path
             fillRule="evenodd"
             clipRule="evenodd"
+            fill="currentcolor"
             d="M0 6v6h4l5 5V1L4 6H0zm7-.17v6.34L4.83 10H2V8h2.83L7 5.83zM13.5 9A4.5 4.5 0 0011
             4.97v8.05c1.48-.73 2.5-2.25 2.5-4.02zM11 .23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5
             6.71v2.06c4.01-.91 7-4.49 7-8.77 0-4.28-2.99-7.86-7-8.77z"
@@ -95,6 +114,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
           <path
             fillRule="evenodd"
             clipRule="evenodd"
+            fill="currentcolor"
             d="M17.502 10c0 .34-.03.66-.07.98l2.11 1.65c.19.15.24.42.12.64l-2
             3.46c-.09.16-.26.25-.43.25-.06 0-.12-.01-.18-.03l-2.49-1c-.52.39-1.08.73-1.69.98l-.38
             2.65c-.03.24-.24.42-.49.42h-4c-.25 0-.46-.18-.49-.42l-.38-2.65c-.61-.25-1.17-.58-1.69-.98l-2.49
@@ -114,22 +134,22 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
     case "add":
       return {
         viewBox: "0 0 68 68",
-        el: <path d="M68 38.857H38.857V68h-9.714V38.857H0v-9.714h29.143V0h9.714v29.143H68v9.714z" />
+        el: <path fill="currentcolor" d="M68 38.857H38.857V68h-9.714V38.857H0v-9.714h29.143V0h9.714v29.143H68v9.714z" />
       };
     case "min2":
       return {
         viewBox: "0 0 12 12",
-        el: <rect width="10" height="2" x="1" y="5"></rect>
+        el: <rect width="10" height="2" x="1" y="5" fill="currentcolor"></rect>
       };
     case "min":
       return {
         viewBox: "0 0 12 12",
-        el: <rect width="10" height="1" x="1" y="6"></rect>
+        el: <rect width="10" height="1" x="1" y="6" fill="currentcolor"></rect>
       };
     case "max":
       return {
         viewBox: "0 0 12 12",
-        el: <rect width="9" height="9" x="1.5" y="1.5" fill="none" stroke="currentColor"></rect>
+        el: <rect width="9" height="9" x="1.5" y="1.5" fill="currentcolor" stroke="currentColor"></rect>
       };
     case "close":
       return {
@@ -138,6 +158,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
           <polygon
             fillRule="evenodd"
             clipRule="evenodd"
+            fill="currentcolor"
             points="11 1.576 6.583 6 11 10.424 10.424 11 6 6.583
             1.576 11 1 10.424 5.417 6 1 1.576 1.576 1 6 5.417 10.424 1"
           ></polygon>
@@ -150,6 +171,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
           <path
             fillRule="evenodd"
             clipRule="evenodd"
+            fill="currentcolor"
             d="M28.5.167L23.506 5.16 43.27 24.957H.167v7.084h43.102L23.506 51.84l4.994 4.993L56.833 28.5 28.5.167z"
           />
         )
@@ -157,7 +179,14 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
     case "chevron":
       return {
         viewBox: "0 0 8 12",
-        el: <path fillRule="evenodd" clipRule="evenodd" d="M1.7 0L.3 1.41 4.88 6 .3 10.59 1.7 12l6-6-6-6z" />
+        el: (
+          <path
+            fillRule="evenodd"
+            fill="currentcolor"
+            clipRule="evenodd"
+            d="M1.7 0L.3 1.41 4.88 6 .3 10.59 1.7 12l6-6-6-6z"
+          />
+        )
       };
     case "clips":
       return {
@@ -166,6 +195,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
           <path
             fillRule="evenodd"
             clipRule="evenodd"
+            fill="currentcolor"
             d="M3 15C3 8.385 8.385 3 15 3s12 5.385 12 12-5.385 12-12 12S3
             21.615 3 15zm-3 0c0 8.28 6.72 15 15 15 8.28 0 15-6.72 15-15
             0-8.28-6.72-15-15-15C6.72 0 0 6.72 0 15zm19.5 0l-6 6V9l6 6z"
@@ -179,6 +209,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
           <path
             fillRule="evenodd"
             clipRule="evenodd"
+            fill="currentcolor"
             d="M14.985 0C6.705 0 0 6.72 0 15c0 8.28 6.705 15 14.985 15C23.28 30 30
             23.28 30 15c0-8.28-6.72-15-15.015-15zM15 27C8.37 27 3 21.63 3 15S8.37 3
             15 3s12 5.37 12 12-5.37 12-12 12zm.75-19.5H13.5v9l7.875 4.725L22.5 19.38l-6.75-4.005V7.5z"
@@ -190,6 +221,7 @@ function getIcon(name: string): { viewBox: string; el: JSX.Element } {
         viewBox: "0 0 512 512",
         el: (
           <path
+            fill="currentcolor"
             d="M496.063 62.299l-46.396-46.4c-21.2-21.199-55.69-21.198-76.888
             0l-18.16 18.161 123.284 123.294 18.16-18.161c21.248-21.249
             21.251-55.643 0-76.894zM22.012 376.747L.251 494.268a15.002
