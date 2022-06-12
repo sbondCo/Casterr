0ff8163c6ba75167022ac6b66aed3f7632c9b20d
IRHM <37304121+IRHM@users.noreply.github.com>
2022-06-12 01:04:16 +0100

    Add onChange to TextBoxProps
    with a discriminated union so the onChange callback newValue type changes depending on input type
    
    and start on Recording settings

--- src/common/TextBox.tsx
+++ src/common/TextBox.tsx
@@ -1,15 +1,38 @@
-interface TextBoxProps {
-  value?: string;
-  placeholder?: string;
+import { useState } from "react";
+
+type TextBoxProps = {
+  value: string | number;
+  placeholder: string | number;
   folderSelect?: boolean;
-}
+} & (
+  | {
+      type?: "text";
+      onChange: (newValue: string) => void;
+    }
+  | {
+      type: "number";
+      onChange: (newValue: number) => void;
+    }
+);
 
 export default function TextBox(props: TextBoxProps) {
-  const { value, placeholder, folderSelect = false } = props;
+  const { value, placeholder, type, folderSelect = false, onChange } = props;
+
+  const [curVal, setCurVal] = useState(value);
 
   return (
     <div className="flex flex-col">
-      <input value={value} type="text" placeholder={placeholder} className="h-8 py-1.5 px-3 bg-secondary-100 rounded" />
+      <input
+        value={curVal}
+        type={type}
+        placeholder={String(placeholder)}
+        onChange={(e) => setCurVal(e.target.value)}
+        onBlur={(e) => {
+          if (type === "text") onChange(e.target.value);
+          else if (type === "number") onChange(Number(e.target.value));
+        }}
+        className="h-8 py-1.5 px-3 bg-secondary-100 rounded"
+      />
     </div>
   );
 }
--- src/settings/pages/Recording.tsx
+++ src/settings/pages/Recording.tsx
@@ -1,18 +1,51 @@
+import { APP_SETTINGS, DEFAULT_SETTINGS } from "@/app/constants";
 import { RootState } from "@/app/store";
-import DropDown from "@/common/DropDown";
+import DropDown, { DropDownItem } from "@/common/DropDown";
+import TextBox from "@/common/TextBox";
+import TickBox from "@/common/TickBox";
 import { useDispatch, useSelector } from "react-redux";
 import SettingsItem from "../SettingsItem";
+import { setFps } from "../settingsSlice";
 
 export default function Recording() {
   const state = useSelector((store: RootState) => store.settings.recording);
   const dispatch = useDispatch();
 
   const videoDevices = ["Default"];
+  const monitors = new Array<DropDownItem>({ id: "primary", name: "Primary Monitor" });
+  const resolutions = ["In-Game", "2160p", "1440p", "1080p", "720p", "480p", "360p"];
 
   return (
     <>
       <SettingsItem title="Video Device">
         <DropDown activeItem={state.videoDevice} items={videoDevices} onChange={(s) => {}} />
+      </SettingsItem>
+
+      <SettingsItem title="Monitor To Record">
+        <DropDown activeItem={state.monitorToRecord} items={monitors} onChange={(s) => {}} />
+      </SettingsItem>
+
+      <SettingsItem title="FPS">
+        <TextBox
+          type="number"
+          value={state.fps}
+          placeholder={DEFAULT_SETTINGS.recording.fps}
+          onChange={(s) => {
+            dispatch(setFps(s));
+          }}
+        />
+      </SettingsItem>
+
+      <SettingsItem title="Resolution">
+        <DropDown activeItem={state.resolution} items={resolutions} onChange={(s) => {}} />
+      </SettingsItem>
+
+      <SettingsItem title="Format">
+        <DropDown activeItem={state.format} items={APP_SETTINGS.supportedRecordingFormats} onChange={(s) => {}} />
+      </SettingsItem>
+
+      <SettingsItem title="Separate Audio Tracks" row>
+        <TickBox ticked={state.seperateAudioTracks} onChange={(t) => {}} />
       </SettingsItem>
     </>
   );
