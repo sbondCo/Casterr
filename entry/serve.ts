ab96d8cb805a3688e056e01b1d9e37198e916b33
IRHM <37304121+IRHM@users.noreply.github.com>
2022-06-14 00:13:53 +0100

    Rebuild entry and restart electron when we detect changes in preload.ts or background.ts files

--- entry/serve.ts
+++ entry/serve.ts
@@ -5,17 +5,20 @@
 
 import { get } from "http";
 import { ChildProcess, exec } from "child_process";
-import { watchFile } from "fs";
+import { Stats, watchFile } from "fs";
+
+type LogPrefix = "srve" | "elec" | "vite";
 
 const NODE_BIN = "./node_modules/.bin";
 const ATTEMPTS = Infinity;
 const DELAY = 500;
 
-let electron: ChildProcess;
+let electron: ChildProcess | undefined;
 let vite: ChildProcess;
+let restarting: boolean = false;
 
 async function start() {
-  log("Starting tasks", "Srve");
+  log("Starting tasks");
 
   vite = exec(`${NODE_BIN}/vite`);
   listen(vite, "vite");
@@ -23,8 +26,48 @@ async function start() {
   electron = await openElectron();
   listen(electron, "elec");
 
+  const restart = async (curr: Stats, prev: Stats, f: string) => {
+    // If already restarting return
+    if (restarting) return;
+
+    if (+curr.mtime - +prev.mtime) {
+      log(`Detected changes in ${f}, restarting Electron.`);
+
+      restarting = true;
+      // const epid = electron!.pid;
+
+      electron!.kill("SIGTERM");
+      await new Promise((r) => setTimeout(r, DELAY));
+
+      log(`Building entry scripts.`);
+      const buildProc = exec("npm run build:entry-scripts");
+      buildProc.on("exit", async () => {
+        electron = undefined;
+        electron = await openElectron();
+        listen(electron, "elec");
+
+        restarting = false;
+        log(`Done.`);
+      });
+
+      // for (let i = 0; i < ATTEMPTS; i++) {
+      //   try {
+      //     // Try kill process, if errors below we know it has exited
+      //     process.kill(epid!, 0);
+      //   } catch (e: any) {
+      //     // ESRCH: process doesn't exist, so we successfully terminated it.
+      //     if (e.code === "ESRCH") break;
+      //     else console.error("Error checking if process killed", e);
+      //   }
+
+      // await new Promise((r) => setTimeout(r, DELAY));
+      // }
+    }
+  };
+
   // TODO: Make this restart app automatically preload or background script changed
-  // watchFile("entry/preload.ts", () => console.log("preload.ts file changed, restart electron for changes"));
+  watchFile("entry/preload.ts", (curr, prev) => restart(curr, prev, "preload.ts"));
+  watchFile("entry/background.ts", (curr, prev) => restart(curr, prev, "background.ts"));
 }
 
 function openElectron() {
@@ -41,10 +84,10 @@ function openElectron() {
         if (resp.statusCode == 200) {
           resolve(exec(`NODE_ENV=dev SERVER_URL=${viteBase} ${NODE_BIN}/electron .`));
         } else {
-          log(`Recieved HTTP code ${resp.statusCode} from vite!`, "Srve");
+          log(`Recieved HTTP code ${resp.statusCode} from vite!`);
         }
       }).on("error", () => {
-        log(`App not available yet, retrying in ${DELAY}ms`, "Srve");
+        log(`App not available yet, retrying in ${DELAY}ms`);
       });
 
       await new Promise((r) => setTimeout(r, DELAY));
@@ -52,17 +95,19 @@ function openElectron() {
   });
 }
 
-async function listen(proc: ChildProcess, prefix: string) {
+async function listen(proc: ChildProcess, prefix: LogPrefix) {
   proc.stdout?.on("data", (chunk) => log(chunk, prefix));
   proc.stderr?.on("data", (chunk) => log(chunk, prefix));
 
   proc.on("close", () => {
-    log(`${prefix} was closed, stopping other tasks.`, "Srve");
-    process.kill(0);
+    if (!restarting) {
+      log(`${prefix} was closed, stopping other tasks.`);
+      process.kill(0);
+    }
   });
 }
 
-async function log(msg: string, prefix: string) {
+async function log(msg: string, prefix: LogPrefix = "srve") {
   const text = msg.split("\n").map((line) => {
     return `[${coloured(prefix)}] ${line}`;
   });
