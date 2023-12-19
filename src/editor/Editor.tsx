514021ea2273fc5c693f294833b1006d63be9611
IRHM <37304121+IRHM@users.noreply.github.com>
2023-12-19 00:59:37 +0000

    Editor: Update delete icon to use trash icon

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -104,7 +104,7 @@ export default function VideoEditor() {
           }}
         />
         <Button
-          icon="close"
+          icon="trash"
           onClick={() => {
             RecordingsManager.deleteWithConfirmation(video)
               .then((deleted) => {
